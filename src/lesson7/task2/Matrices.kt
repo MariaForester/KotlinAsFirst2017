234ab74f37dbe1d3b55d8c16e8723562f55790ba
Amario Forester <maschalessnichaya@gmail.com>
2017-12-15 17:15:59 +0300

    comments 5 && 6

--- src/lesson7/task2/Matrices.kt
+++ src/lesson7/task2/Matrices.kt
@@ -176,27 +176,8 @@ fun generateSnake(height: Int, width: Int): Matrix<Int> = TODO()
  * 4 5 6      8 5 2
  * 7 8 9      9 6 3
  */
-fun <E> rotate(matrix: Matrix<E>): Matrix<E> {
-    if (matrix.height != matrix.width) {
-        throw IllegalArgumentException()
-    }
-    val result = matrix
-    for (i in 0 until result.height) {
-        for (j in 0 until result.height - 1 - i) {
-            val component = result[i, j]
-            result[i, j] = result[result.height - 1 - j, result.height - 1 - i]
-            result[result.height - 1 - j, result.height - 1 - i] = component
-        }
-    }
-    for (i in 0 until result.height / 2) {
-        for (j in 0 until result.height) {
-            val rsquareSubstitute = result[i, j]
-            result[i, j] = result[result.height - 1, j]
-            result[result.height - 1 - i, j] = rsquareSubstitute
-        }
-    }
-    return result
-}
+fun <E> rotate(matrix: Matrix<E>): Matrix<E> = TODO()
+
 
 /**
  * Сложная
